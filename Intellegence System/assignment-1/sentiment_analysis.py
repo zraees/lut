a95eaf38908b8442e2fdced2649752901efcc245
zeeshanrais <developerxone@hotmail.com>
2025-02-10 20:46:40 +0200

    added readme file

--- Intellegence System/assignment-1/sentiment_analysis.py
+++ Intellegence System/assignment-1/sentiment_analysis.py
@@ -11,14 +11,12 @@ from nltk.tokenize import word_tokenize
 # Initial setup: Download necessary NLTK data (if not already downloaded)
 try:
     nltk.data.find('tokenizers/punkt')
-except LookupError:
-    print("Downloading NLTK tokenizer...")
+except LookupError: 
     nltk.download('punkt')
 
 try:
     nltk.data.find('tokenizers/punkt_tab')
-except LookupError:
-    print("Downloading NLTK tokenizer...")
+except LookupError: 
     nltk.download('punkt_tab')
         
 # nltk.download('punkt')
@@ -26,37 +24,37 @@ except LookupError:
 
 # Step to Load Train and Test Datasets
 print("Loading training and testing datasets...")
-train_df = pd.read_csv("IMDB Dataset Train.csv")
-test_df = pd.read_csv("IMDB Dataset Test.csv")
+imdb_train_df = pd.read_csv("IMDB Dataset Train.csv")
+imdb_test_df = pd.read_csv("IMDB Dataset Test.csv")
 
 # Step to Preprocess Text Data (1- Tokenize & lowercase, 2- Remove non-alphanumeric)
 def preprocess_text(text):
     tokens = word_tokenize(text.lower())  
     return ' '.join([word for word in tokens if word.isalnum()])
 
-train_df['clean_text'] = train_df['text'].apply(preprocess_text)
-test_df['clean_text'] = test_df['text'].apply(preprocess_text)
+imdb_train_df['clean_text'] = imdb_train_df['text'].apply(preprocess_text)
+imdb_test_df['clean_text'] = imdb_test_df['text'].apply(preprocess_text)
 
 # Step to Split Data in varaibles
-X_train = train_df['clean_text']
-y__train = train_df['label']
-X_test = test_df['clean_text']
-y__test = test_df['label']
+X__train = imdb_train_df['clean_text']
+y__train = imdb_train_df['label']
+X__test = imdb_test_df['clean_text']
+y__test = imdb_test_df['label']
 
 # Step to Feature Extraction with TF-IDF (suggested in excercise class)
-vectorizer_raw = TfidfVectorizer()
-vectorizer_stop = TfidfVectorizer(stop_words="english")
+vectorizer__raw = TfidfVectorizer()
+vectorizer__stop = TfidfVectorizer(stop_words="english")
 
-X__train_tfidf = vectorizer_raw.fit_transform(X_train)
-X__test_tfidf = vectorizer_raw.transform(X_test)
+X__train_tfidf = vectorizer__raw.fit_transform(X__train)
+X__test_tfidf = vectorizer__raw.transform(X__test)
 
-X__train_tfidf_stop = vectorizer_stop.fit_transform(X_train)
-X__test_tfidf_stop = vectorizer_stop.transform(X_test)
+X__train_tfidf_stop = vectorizer__stop.fit_transform(X__train)
+X__test_tfidf_stop = vectorizer__stop.transform(X__test)
 
 # Important Step; Train Logistic Regression Models ( approach 1 & 2 )
 log__reg = LogisticRegression(max_iter=1000)
 log__reg.fit(X__train_tfidf, y__train)
-y_pred_tfidf = log__reg.predict(X__test_tfidf)
+y__pred_tfidf = log__reg.predict(X__test_tfidf)
 
 log__reg_stop = LogisticRegression(max_iter=1000)
 log__reg_stop.fit(X__train_tfidf_stop, y__train)
@@ -66,10 +64,10 @@ y__pred_tfidf_stop = log__reg_stop.predict(X__test_tfidf_stop)
 fasttext_train_txt_file = "fasttext_train.txt"
 
 if not os.path.exists(fasttext_train_txt_file):
-    print("Formatting data for FastText ...")
+    print("Generating data for FastText ...")
     
-    train_df['fasttext_label'] = '__label__' + train_df['label'].astype(str)
-    train_df[['fasttext_label', 'clean_text']].to_csv(
+    imdb_train_df['fasttext_label'] = '__label__' + imdb_train_df['label'].astype(str)
+    imdb_train_df[['fasttext_label', 'clean_text']].to_csv(
         fasttext_train_txt_file,
         index=False,
         header=False,
@@ -82,12 +80,12 @@ if not os.path.exists(fasttext_train_txt_file):
 ft_model = fasttext.train_supervised(
     input=fasttext_train_txt_file,
     lr=0.01,         # set learning rate for better convergence
-    epoch=150,       # Increase epochs for better training
+    epoch=150,       # Inc epochs for better training
     wordNgrams=3,    # Use 3-grams for capturing more context
-    dim=500,         # Increase dimension for richer word vectors
-    loss='softmax',  # Change loss function for better results in text classification
-    bucket=2000000,  # Increase the bucket size for better handling of larger vocab
-    minCount=5       # Consider only words with frequency higher than 5
+    dim=500,         # Inc dimension for richer word vectors
+    loss='softmax',  # loss function for better results in text classification
+    bucket=2000000,  # Inc the bucket size for better handling of larger vocab
+    minCount=5       # only words with frequency higher than 5
 )
 
 # sample_text = "__label__1 This is a sample review."
@@ -125,7 +123,7 @@ def predict_with_fasttext(model, texts):
     return predictions
 
 # Predict on test data
-y__pred_ft = predict_with_fasttext(ft_model, X_test)
+y__pred_ft = predict_with_fasttext(ft_model, X__test)
 
 # Print predictions
 #print("FastText Predictions:", y__pred_ft[:10])  # Displaying the first 10 predictions
@@ -141,8 +139,8 @@ def evaluate_model(y_true, y_pred, model_name):
     except ValueError as e:
         print(f"Error in {model_name} evaluation: {e}")
 
-evaluate_model(y__test, y_pred_tfidf, "Logistic Regression (TF-IDF)")
-evaluate_model(y__test, y__pred_tfidf_stop, "Logistic Regression (TF-IDF & Stopword Filtering)")
+evaluate_model(y__test, y__pred_tfidf, "Logistic Regression (TF-IDF) Model")
+evaluate_model(y__test, y__pred_tfidf_stop, "Logistic Regression (TF-IDF & Stopword Filtering) Model")
 evaluate_model(y__test, y__pred_ft, "FastText Model")
 
-print("\n Sentiment Analysis on IMDb Completed Successfully!")
+print("\n Sentiment Analysis on IMDb Done Successfully!")
